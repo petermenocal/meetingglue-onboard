0aff9141ad41ce377ca7f0feacaf689c138f5436
Peter Menocal <peter@geekypixel.com>
2018-11-14 09:37:22 -0800

    update

--- src/components/EnquiryFormSimpleAjax.js
+++ src/components/EnquiryFormSimpleAjax.js
@@ -73,27 +73,304 @@ class Form extends React.Component {
           <input
             className='EnquiryForm--Input'
             type='text'
-            placeholder='Name'
-            name='name'
+            placeholder='Venue Name'
+            name='venue_name'
             required
           />
         </label>
         <label className='EnquiryForm--Label'>
-          <textarea
-            className='EnquiryForm--Input EnquiryForm--Textarea'
-            placeholder='Message'
-            name='message'
-            rows='10'
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Venue Address'
+            name='venue_address'
             required
           />
         </label>
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Sales office phone'
+            name='venue_sales_office_phone'
+            required
+          />
+        </label>
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Sales office email'
+            name='venue_sales_office_email'
+            required
+          />
+        </label>
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Rating'
+            name='venue_rating'
+            required
+          />
+        </label>
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Chain'
+            name='venue_chain'
+            required
+          />
+        </label>
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Nearest airport'
+            name='venue_nearest_airport'
+            required
+          />
+        </label>
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Loyalty program name'
+            name='venue_loyalty_program_name'
+            required
+          />
+        </label>
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Union or non-union?'
+            name='venue_union_status'
+            required
+          />
+        </label>
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Sleeping rooms totals'
+            name='venue_sleeping_rooms_totals'
+            required
+          />
+        </label>
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Rooms tax'
+            name='venue_rooms_tax'
+            required
+          />
+        </label>
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Resort Fee'
+            name='venue_resort_fee'
+            required
+          />
+        </label>
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Wifi price'
+            name='venue_wifi_price'
+            required
+          />
+        </label>
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Onsite parking fee'
+            name='venue_onsite_parking_fee'
+            required
+          />
+        </label>
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Meeting Space Total'
+            name='venue_meeting_space_total'
+            required
+          />
+        </label>
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Meeting Space: Largest Room 1'
+            name='venue_meeting_space_largest_1'
+            required
+          />
+        </label>
+
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Meeting Space: Largest Room 2'
+            name='venue_meeting_space_largest_2'
+            required
+          />
+        </label>
+
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Number of meeting rooms'
+            name='venue_number_of_meeting_rooms'
+            required
+          />
+        </label>
+
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Public Wifi Space Fee'
+            name='venue_public_wifi_space_fee'
+            required
+          />
+        </label>
+
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Inhouse AV Company'
+            name='venue_inhouse_av_company'
+            required
+          />
+        </label>
+
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Business Center'
+            name='venue_business_center'
+            required
+          />
+        </label>
+
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Sales Tax'
+            name='venue_sales_tax'
+            required
+          />
+        </label>
+
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Service Charge'
+            name='venue_service_charge'
+            required
+          />
+        </label>
+
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Custom menu availability?'
+            name='venue_custom_menu'
+            required
+          />
+        </label>
+
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Restaurants Onsite Total'
+            name='venue_restaurants_onsite'
+            required
+          />
+        </label>
+
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Fitness Center? (Y/N)'
+            name='venue_fitness_center'
+            required
+          />
+        </label>
+
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Pool?'
+            name='venue_pool'
+            required
+          />
+        </label>
+
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Spa?'
+            name='venue_spa'
+            required
+          />
+        </label>
+
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Golf?'
+            name='venue_golf'
+            required
+          />
+        </label>
+
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Pet Policy'
+            name='venue_pet_policy'
+            required
+          />
+        </label>
+
+        <label className='EnquiryForm--Label'>
+          <input
+            className='EnquiryForm--Input'
+            type='text'
+            placeholder='Airport shuttle'
+            name='venue_airport_shuttle'
+            required
+          />
+        </label>
+
         <input type='text' name='_gotcha' style={{ display: 'none' }} />
         {!!subject && <input type='hidden' name='subject' value={subject} />}
         <input type='hidden' name='form-name' value={name} />
         <input
           className='Button EnquiryForm--SubmitButton'
           type='submit'
-          value='Enquire'
+          value='Submit'
           disabled={this.state.disabled}
         />
       </form>
